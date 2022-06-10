25f9468447fa4cb07c7fbf6cf25ea75f9a74990a
mikafung <mika.fung@asiatop.asia>
2022-06-10 10:22:12 +0800

    initial

---
+++ src/main/java/hkpl_backend/hkpl_backend/HkplBackendApplication.java
@@ -0,0 +1,13 @@
+package hkpl_backend.hkpl_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkplBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HkplBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hkpl_backend/hkpl_backend/HkplBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hkpl_backend.hkpl_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkplBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
